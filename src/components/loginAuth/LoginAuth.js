16fe057b8a52ed0f66b4de5a03519585283c1697
shobhit jaryal <jaryalshaurya2203@gmail.com>
2021-11-08 16:26:31 +0530

    AlphaV2

--- src/components/loginAuth/LoginAuth.js
+++ src/components/loginAuth/LoginAuth.js
@@ -1,28 +1,38 @@
-let root=document.getElementById("root")
-let pass=document.createElement("input");
-pass.setAttribute('type','password');
-let btn=document.createElement("button");
-btn.classList.add('btnAdmin');
-btn.addEventListener("click",()=>checkpass());
-btn.innerHTML="Login"
-function checkpass(){
-    if(pass.value=="1234"){
-        localStorage.setItem("admin",JSON.stringify(true))
-        window.location.href="../admin/Admin.html"
-    }
+let root = document.getElementById("root");
+let pass = document.createElement("input");
+pass.setAttribute("type", "password");
+let btn = document.createElement("button");
+btn.classList.add("btn-danger");
+btn.classList.add("btn");
+btn.classList.add("btnAdmin");
+btn.addEventListener("click", () => checkpass());
+btn.innerHTML = "Login";
+function checkpass() {
+  if (pass.value == "1234") {
+    localStorage.setItem("admin", JSON.stringify(true));
+    window.location.href = "../admin/Admin.html";
+  } else {
+    // alert("booooo you wrong?")
+    // if(document.getElementById('alrt')){
+    //     return;
+    // }
 
-    else{
-        // alert("booooo you wrong?")
-        let alr = document.createElement('div');
-        alr.setAttribute('role','alert')
-        alr.classList.add('alert');
-        alr.classList.add('alert-warning');
-        alr.classList.add('animate__animated', 'animate__headShake');
-        alr.innerHTML = 'Wrong Password! Try Again';
-        root.append(alr);
-        
-    }
+
+    let alr = document.createElement("div");
+    alr.setAttribute('id','alrt');
+    alr.setAttribute("role", "alert");
+    alr.classList.add("alert");
+    alr.classList.add("alert-warning");
+    alr.classList.add("animate__animated", "animate__headShake");
+    alr.innerHTML = "Wrong Password! Try Again";
+    root.append(alr);
+
+    setTimeout(()=>{
+        root.removeChild(alr);
+    },4000)
+
+  }
 }
 
 root.append(pass);
-root.append(btn)
+root.append(btn);
